c42e0f6896cb2e41e2b81bf52c7e25529eb4d93a
Shreya <shreya11072003@gmail.com>
2022-05-27 15:45:34 +0530

    commit

--- exercise.js
+++ exercise.js
@@ -87,3 +87,36 @@ for (let i = 0; i < hor.length; i++) {
   horver.push([hor[i].id,ver[i].id].join(','))
   
 }
+
+
+function makeDraggable(evt) {
+  var selectedElement = false;
+  var svg = evt.target;
+  svg.addEventListener('mousedown', startDrag);
+  svg.addEventListener('mousemove', drag);
+  svg.addEventListener('mouseup', endDrag);
+  svg.addEventListener('mouseleave', endDrag);
+  function startDrag(evt) {
+    if (evt.target.classList.contains('draggable')) {
+      selectedElement = evt.target;
+    }
+  }
+  function drag(evt) {
+  if (selectedElement) {
+    evt.preventDefault();
+    var coord = getMousePosition(evt);
+    selectedElement.setAttributeNS(null, "x", coord.x);
+    selectedElement.setAttributeNS(null, "y", coord.y);
+  }
+}
+  function endDrag(evt) {
+    selectedElement = null;
+  }
+  function getMousePosition(evt) {
+    var CTM = svg.getScreenCTM();
+    return {
+      x: (evt.clientX - CTM.e) / CTM.a,
+      y: (evt.clientY - CTM.f) / CTM.d
+    };
+  }
+}
